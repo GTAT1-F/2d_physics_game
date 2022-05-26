ae26cff24c9c9ad23b10e6e9bbcf6c26ec18117e
RS9551 <104462266+RS9551@users.noreply.github.com>
2022-05-26 19:25:21 +0200

    Random attack sounds added

--- Assets/Scripts/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BulletSpawner : MonoBehaviour
 {
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject bulletPrefab;
 
+    private const int AMOUNT_ATTACK_SOUNDS = 4;
+    [SerializeField] private AudioSource[] attackSound = new AudioSource[AMOUNT_ATTACK_SOUNDS];
+    
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
+
+            int randomIndex = Random.Range(0, 5);
+            attackSound[randomIndex].Play();
         }
     }
 
